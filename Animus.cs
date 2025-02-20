728a26708a332b77d6f9fd3819d6ac7c981f5ce1
Begi <gi.begiashvili@ssu.edu.ge>
2025-02-21 02:22:05 +0400

    first commit

---
+++ Animus.cs
@@ -0,0 +1,13 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace Animus;
+
+public class Animus : BasePlugin
+{
+    public override string ModuleName => "Animus";
+    public override string ModuleVersion => "0.0.1";
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
